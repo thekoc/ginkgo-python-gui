d166192bfbab48887676a51cc1d05de6c24e16a6
thekoc <koc@SmartPillow.local>
2016-07-01 20:18:35 +0800

    格式改动

--- IDATdb.py
+++ IDATdb.py
@@ -2,6 +2,7 @@
 
 import pyodbc
 
+
 class ConnectionError(Exception):
     def __init__(self, value='Failed to connect'):
         self.value = value
@@ -15,11 +16,15 @@ class Database(object):
         self.database_name = 'IDATDB'
         self.port = 1433
         self.driver = 'FreeTDS'
+        self.connected = False
+        self.odbc_connection = None
 
     def connect(self, uid, pwd, server):
         try:
-            self.connect = pyodbc.connect('DRIVER=%s;SERVER=%s;PORT=%d;DATABASE=%s;UID=%s;PWD=%s' %\
+            self.odbc_connection = pyodbc.connect('DRIVER=%s;SERVER=%s;PORT=%d;DATABASE=%s;UID=%s;PWD=%s' %\
                                          (self.driver, server, self.port, self.database_name, uid, pwd))
         except pyodbc.Error:
             raise ConnectionError
+        else:
+            self.connected = True
 
