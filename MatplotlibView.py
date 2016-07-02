36c7790caada6cc06ca969e7ae7fc43d350a826d
thekoc <koc@SmartPillow.local>
2016-07-02 23:41:43 +0800

    Add a matplotlib view

---
+++ MatplotlibView.py
@@ -0,0 +1,34 @@
+import matplotlib
+matplotlib.use("WxAgg")
+
+import wx
+
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
+from matplotlib.backends.backend_wx import NavigationToolbar2Wx, wxc
+from matplotlib.figure import Figure
+
+class MatplotlibPanel(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+        self.figure = Figure()
+        self.axes = self.figure.add_subplot(111)
+        self.canvas = FigureCanvas(self, -1, self.figure)
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.canvas, 1, wx.LEFT | wx.TOP | wx.GROW)
+        self.add_toolbar()
+
+        self.SetSizer(self.sizer)
+        self.Fit()
+
+    def add_toolbar(self):
+        self.toolbar = NavigationToolbar2Wx(self.canvas)
+        self.toolbar.Realize()
+        self.sizer.Add(self.toolbar, 0, wx.ALIGN_CENTER | wx.EXPAND)
+        self.toolbar.update()
+
+if __name__ == '__main__':
+    app = wx.App()
+    frame = wx.Frame(None, title='demo app')
+    p = MatplotlibPanel(frame)
+    frame.Show(True)
+    app.MainLoop()
